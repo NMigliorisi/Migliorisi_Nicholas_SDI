0d285f8f133a2e754be70c5bb1ad4b2856c05fed
NMigliorisi <migz517@fullsail.edu>
2015-06-03 17:41:46 -0400

    variables added

--- Migliorisi_Nicholas_Output_Assignment/js/Migliorisi_Nicholas_Output_Assignment.js
+++ Migliorisi_Nicholas_Output_Assignment/js/Migliorisi_Nicholas_Output_Assignment.js
@@ -4,5 +4,13 @@
    WDDBS
         */
 
+var favoriteMusic = ("I'm a big fan of Green Day, Led Zeppelin, and Ed Sheeran"); //Variable for String
+
+var familyMembers = 5; //Variable for number
+
+var isEmployed = true; //Variable for Boolean
+
+
+
 
 
