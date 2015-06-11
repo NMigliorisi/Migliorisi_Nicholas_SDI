dfb317bd7a9e4a90aee61cf3af2b43f48ca92f8c
NMigliorisi <migz517@fullsail.edu>
2015-06-11 12:05:12 -0400

    Beautified the comments and code

--- Expressions_Assignmnet/Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_assignment.js
+++ Expressions_Assignmnet/Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_assignment.js
@@ -1,14 +1,13 @@
-//Nicholas Migliorisi  06/11/15 Expressions assignment
+//Nicholas Migliorisi  06/11/15 Expressions assignment WDDBS Week 2 SDI
 
-var lengthInput = prompt("We are finding the density of a box with a mass of 100 kilograms. \nPlease insert the length of the box:"); //Gathering the length
-var widthInput = prompt("Please insert the width:"); // Gathering the width
-var heightInput = prompt("Please insert the height:"); //Gathering the height
+var lengthInput = prompt("We are finding the density of a box with a mass of 100 kilograms. \nPlease insert the length of the box:");                                                     //Gathering the length
+var widthInput = prompt("Please insert the width:");         // Gathering the width
+var heightInput = prompt("Please insert the height:");       //Gathering the height
 
-var volume = lengthInput * widthInput * heightInput; //Finding volume
-var mass = 100; //Determining the mass
+var volume = lengthInput * widthInput * heightInput;         //Finding volume
+var mass = 100;                                              //Determining the mass
 
-var density = mass / volume; //finding density
-//console.log(density);// prints it to the console
-
+var density = mass / volume;                                 //finding density
+//console.log(density);                                      // prints it to the console
 alert("The density of our box is "+ density + "kg/m cubed"); //displays answer to user
 
