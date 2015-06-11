333bbea0bf50ca70d95672d70fed502070693575
NMigliorisi <migz517@fullsail.edu>
2015-06-11 11:49:44 -0400

    Finding volume
    in order to find the density we need to find volume

--- Expressions_Assignmnet/Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_assignment.js
+++ Expressions_Assignmnet/Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_assignment.js
@@ -1,7 +1,12 @@
 //Nicholas Migliorisi  06/11/15 Expressions assignment
 
-prompt("We are finding the mass of a box with a density of 5 pounds. \nPlease insert the length of the box:"); //Gathering the length
-prompt("Please insert the width:"); // Gathering the width
-prompt("Please insert the height:"); //Gathering the height
+var lengthInput = prompt("We are finding the density of a box with a mass of 35 pounds. \nPlease insert the length of the box:"); //Gathering the length
+var widthInput = prompt("Please insert the width:"); // Gathering the width
+var heightInput = prompt("Please insert the height:"); //Gathering the height
+
+var volume = lengthInput * widthInput * heightInput; //Finding volume
+
+
+
 
 
