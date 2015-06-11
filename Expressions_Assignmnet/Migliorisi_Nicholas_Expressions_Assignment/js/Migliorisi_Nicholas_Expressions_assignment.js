069fcaf2581092c58c219fc8c1d4fb3ee8a03668
NMigliorisi <migz517@fullsail.edu>
2015-06-11 11:36:54 -0400

    Setting up prompts

---
+++ Expressions_Assignmnet/Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_assignment.js
@@ -0,0 +1,7 @@
+//Nicholas Migliorisi  06/11/15 Expressions assignment
+
+prompt("We are finding the mass of a box with a density of 5 pounds. \nPlease insert the length of the box:"); //Gathering the length
+prompt("Please insert the width:"); // Gathering the width
+prompt("Please insert the height:"); //Gathering the height
+
+
