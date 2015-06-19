7f5a9cf6f88c609b780d3b06cc20532dd7ca1de6
NMigliorisi <migz517@fullsail.edu>
2015-06-18 21:06:02 -0400

    adding prompts

--- Migliorisi_Nicholas_Conditionals_Assignment/js/Migliorisi_Nicholas_Conditionals_Assignment.js
+++ Migliorisi_Nicholas_Conditionals_Assignment/js/Migliorisi_Nicholas_Conditionals_Assignment.js
@@ -1,2 +1,6 @@
+//Nicholas Migliorisi Conditionals Assignment SDI Week 3 WDDBS 06/18/2015
 
+prompt("We are attempting to calculate the average price of concert tickets to decide a price cut. \n Please insert a ticket price you have purchased:";                         //Gathering info for input 1
+prompt("Insert another price you have paid:");              //Gathering info for input 2
+prompt("Insert a price a friend or relative has paid:");    //Gathering info for input 3
 
