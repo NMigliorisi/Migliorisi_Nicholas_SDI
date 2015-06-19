66aef3cb2536f8ad1b6f0f016f2568bc8adf0c00
NMigliorisi <migz517@fullsail.edu>
2015-06-18 21:23:45 -0400

    Adding conditionals

--- Migliorisi_Nicholas_Conditionals_Assignment/js/Migliorisi_Nicholas_Conditionals_Assignment.js
+++ Migliorisi_Nicholas_Conditionals_Assignment/js/Migliorisi_Nicholas_Conditionals_Assignment.js
@@ -1,6 +1,23 @@
 //Nicholas Migliorisi Conditionals Assignment SDI Week 3 WDDBS 06/18/2015
 
-prompt("We are attempting to calculate the average price of concert tickets to decide a price cut. \n Please insert a ticket price you have purchased:";                         //Gathering info for input 1
-prompt("Insert another price you have paid:");              //Gathering info for input 2
-prompt("Insert a price a friend or relative has paid:");    //Gathering info for input 3
+var ticketOne = prompt("We are attempting to calculate the average price of concert tickets to decide a price cut. \n Please insert a ticket price you have purchased:");                         //Gathering info for input 1
+var ticketTwo = prompt("Insert another price you have paid:");              //Gathering info for input 2
+var ticketThree = prompt("Insert a price a friend or relative has paid:");    //Gathering info for input 3
+var forgotPrice = ("You forgot to tell me a price")
+if(ticketOne == ""){
+    prompt(forgotPrice)
+}else{
+    ticketTwo
+};
 
+if(ticketTwo == ""){
+    prompt(forgotPrice)
+}else{
+    ticketThree
+};
+
+if(ticketThree == ""){
+    prompt(forgotPrice)
+}else{
+ alert("Thank You!")
+};
