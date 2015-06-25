da881b330875cf0229cc711e074f52f0e9f812d6
NMigliorisi <migz517@fullsail.edu>
2015-06-25 12:56:51 -0400

    Figured out my loop issue

--- Migliorisi_Nicholas_Functions_Assignment/js/Migliorisi_Nicholas_Functions_Assignment.js
+++ Migliorisi_Nicholas_Functions_Assignment/js/Migliorisi_Nicholas_Functions_Assignment.js
@@ -4,6 +4,23 @@
 
 
 
+while(true){                     //Starting a loop to validate the user input
+    var gameType = prompt("What game would you like to play? \nFlorida State Lottery or Powerball?"); //colecting the data from the user
+if(gameType == "Powerball"){          //If the user selects powerball then the loop breaks and it prints to the console
+    console.log("Powerball")
+    break;
+}else{                               //If the user doesn't select Powerball then the code looks here
+    if(gameType == "Florida State Lottery"){        //If the user picks Florida state Lottery then its printed to the console and the loop breaks
+        console.log ("Florida State Lottery")
+        break;
+    }
+    }
+    if(gameType != "Powerball" || "Florida State Lottery"){   //If neither Powerball nor Florida State Lottery is chosen, the user is alerted and the loop goes back to the start.
+        alert("Thats not a valid game. Try again!")   //If none are selected and the user says PA lottery, for example, this yells at them.
+    }
+}
+
+
 
 
 
