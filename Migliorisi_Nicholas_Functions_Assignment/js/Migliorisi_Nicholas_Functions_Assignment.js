54b4a461f19f51067ae2ec9ec4cccce92be6f76b
NMigliorisi <migz517@fullsail.edu>
2015-06-25 14:13:59 -0400

    attempting to pull random numbers

--- Migliorisi_Nicholas_Functions_Assignment/js/Migliorisi_Nicholas_Functions_Assignment.js
+++ Migliorisi_Nicholas_Functions_Assignment/js/Migliorisi_Nicholas_Functions_Assignment.js
@@ -19,6 +19,34 @@ if(gameType == "Powerball"){          //If the user selects powerball then the l
         alert("Thats not a valid game. Try again!")   //If none are selected and the user says PA lottery, for example, this yells at them.
     }
 }
+if(gameType = "Powerball") {
+    var pwrStart = 1
+    var pwrEnd = 59
+    var numbers = new Array();
+    for (n = pwrStart; n <= pwrEnd; n++) {
+        numbers.push(n);
+    }
+
+    function powerNumbers() {
+        var n = this.length, m, temp;
+        if (n === 0) return false;
+        while (--n) {
+            n = Math.floor(Math.random() * ( n + 1 ));
+            temp = this[m];
+            this[n] = this[m];
+            this[m] = temp;
+        }
+    }
+
+    Array.prototype.shuffle = powerNumbers
+    numbers.shuffle();
+
+    numbers.pop();
+    numbers.pop();
+    numbers.pop();
+    numbers.pop();
+    numbers.pop();
+}
 
 
 
